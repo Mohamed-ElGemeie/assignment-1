58c805c4f61c930ffe87175b3f3ce691b1a9f8c4
Hazemkhaled1101 <148864543+Hazemkhaled1101@users.noreply.github.com>
2023-10-25 21:24:44 +0300

    Revert "Delete assignment gamil.py"
    This reverts commit 52d910aa1d0d62bd1875b35ebc54ff33bf76730c.

---
+++ assignment gamil.py
@@ -0,0 +1,20 @@
+def generate_fibonacci(N):
+    if N <= 0:
+        return "Enter a positive number for N."
+
+    fib_sequence = [0, 1]
+
+    while len(fib_sequence) < N:
+        next_number = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_number)
+
+    return " ".join(map(str, fib_sequence[:N]))
+def count_vowels(input_string):
+    vowels = "aeiouAEIOU"
+    vowel_count = 0
+
+    for char in input_string:
+        if char in vowels:
+            vowel_count += 1
+
+    return vowel_count
